e78e86d3da3b9684453be7e94729de361c091c93
SpiderSaifan <103812103+yfusdgysgyjgsysyejgysd@users.noreply.github.com>
2025-10-04 14:35:30 +0600

    Added new variables, functions, classes and FPS counter but everything else is the same.

---
+++ src/com/base/engine/Game.java
@@ -0,0 +1,24 @@
+package com.base.engine;
+
+public class Game
+{
+    public Game()
+    {
+
+    }
+
+    public void input()
+    {
+
+    }
+
+    public void update()
+    {
+
+    }
+
+    public void render()
+    {
+
+    }
+}
--- src/com/base/engine/MainComponent.java
+++ src/com/base/engine/MainComponent.java
@@ -4,13 +4,16 @@ public class MainComponent
 {
     public static final int WIDTH = 1200;
     public static final int HEIGHT = 900;
-    public static final String TITLE = "3d engine why am i wasting precious hours of my life doing this";
+    public static final String TITLE = "3d engine ahh 61 episodes";
+    public static final double FRAME_CAP = 5000.0;
 
     private boolean isRunning;
+    private Game game;
 
     public MainComponent()
     {
         isRunning = false;
+        game = new Game();
     }
 
     public void start()
@@ -33,12 +36,59 @@ public class MainComponent
     {
         isRunning = true;
 
+        int frames = 0;
+        long frameCounter = 0;
+
+        final double frameTime = 1.0 / FRAME_CAP;
+
+        long lastTime = Time.getTime();
+        double unprocessedTime = 0;
+
         while(isRunning)
         {
-            if(Window.isCloseRequested())
-                stop();
+            boolean render = false;
 
-            render();
+            long startTime = Time.getTime();
+            long passedTime = startTime - lastTime;
+            lastTime = startTime;
+
+            unprocessedTime += passedTime / (double)Time.SECOND;
+            frameCounter += passedTime;
+
+            while(unprocessedTime > frameTime)
+            {
+                render = true;
+
+                unprocessedTime -= frameTime;
+
+                if(Window.isCloseRequested())
+                    stop();
+
+                game.input();
+                game.update();
+
+                if(frameCounter >= Time.SECOND)
+                {
+                    System.out.println(frames);
+                    frames = 0;
+                    frameCounter = 0;
+                }
+            }
+            if(render)
+            {
+                render();
+                frames++;
+            }
+            else
+            {
+                try {
+                    Thread.sleep(1);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
         }
 
         cleanUp();
@@ -46,6 +96,7 @@ public class MainComponent
 
     private void render()
     {
+        game.render();
         Window.render();
     }
 
