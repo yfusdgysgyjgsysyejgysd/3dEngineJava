cc95e83142eacde7b83f0c45c7bfe18b0de65972
SpiderSaifan <103812103+yfusdgysgyjgsysyejgysd@users.noreply.github.com>
2025-10-04 14:11:34 +0600

    Made a Window

--- src/com/base/engine/MainComponent.java
+++ src/com/base/engine/MainComponent.java
@@ -1,4 +1,65 @@
 package com.base.engine;
 
-public class MainComponent {
+public class MainComponent
+{
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 900;
+    public static final String TITLE = "3d engine why am i wasting precious hours of my life doing this";
+
+    private boolean isRunning;
+
+    public MainComponent()
+    {
+        isRunning = false;
+    }
+
+    public void start()
+    {
+        if(isRunning)
+            return;
+
+        run();
+    }
+
+    public void stop()
+    {
+        if(!isRunning)
+            return;
+
+        isRunning = false;
+    }
+
+    private void run()
+    {
+        isRunning = true;
+
+        while(isRunning)
+        {
+            if(Window.isCloseRequested())
+                stop();
+
+            render();
+        }
+
+        cleanUp();
+    }
+
+    private void render()
+    {
+        Window.render();
+    }
+
+    private void cleanUp()
+    {
+        Window.dispose();
+    }
+
+    public static void main(String[] args)
+    {
+        Window.createWindow(WIDTH, HEIGHT, TITLE);
+
+        MainComponent game = new MainComponent();
+
+        game.start();
+    }
 }
