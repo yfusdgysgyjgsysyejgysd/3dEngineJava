89f8f3e27774542f335be905fc21c22e315b1a3e
SpiderSaifan <103812103+yfusdgysgyjgsysyejgysd@users.noreply.github.com>
2025-10-13 17:25:57 +0600

    Added Shader Compilation

--- src/com/base/engine/Shader.java
+++ src/com/base/engine/Shader.java
@@ -1,4 +1,81 @@
 package com.base.engine;
 
-public class Shader {
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL32.*;
+
+public class Shader
+{
+    private int program;
+
+    public Shader()
+    {
+        program = glCreateProgram();
+
+        if(program == 0)
+        {
+            System.err.println("shader creation no no :(. no good memry location in constructor");
+            System.exit(1);
+        }
+    }
+
+    public void bind()
+    {
+        glUseProgram(program);
+    }
+
+    public void addVertexShader(String text)
+    {
+        addProgram(text, GL_VERTEX_SHADER);
+    }
+
+    public void addGeometryShader(String text)
+    {
+        addProgram(text, GL_GEOMETRY_SHADER);
+    }
+
+    public void addFragmentShader(String text)
+    {
+        addProgram(text, GL_FRAGMENT_SHADER);
+    }
+
+    public void compileShader()
+    {
+        glLinkProgram(program);
+
+        if(glGetShader(program, GL_LINK_STATUS) == 0)
+        {
+            System.err.println(glGetShaderInfoLog(program, 1024));
+            System.exit(1);
+        }
+
+        glValidateProgram(program);
+
+        if(glGetShader(program, GL_VALIDATE_STATUS) == 0)
+        {
+            System.err.println(glGetShaderInfoLog(program, 1024));
+            System.exit(1);
+        }
+    }
+
+    private void addProgram(String text, int type)
+    {
+        int shader = glCreateShader(type);
+
+        if(shader == 0)
+        {
+            System.err.println("shader creation no no :(. no good memry location when shader add add");
+            System.exit(1);
+        }
+
+        glShaderSource(shader, text);
+        glCompileShader(shader);
+
+        if(glGetShader(shader, GL_COMPILE_STATUS) == 0)
+        {
+            System.err.println(glGetShaderInfoLog(shader, 1024));
+            System.exit(1);
+        }
+
+        glAttachShader(shader, program);
+    }
 }
